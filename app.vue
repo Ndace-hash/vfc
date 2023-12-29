4ad69eb07d104b1acfedade4fb9286a6df142705
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2023-12-29 08:48:32 +0100

    added routing

--- app.vue
+++ app.vue
@@ -1,4 +1,5 @@
 <template>
-  <div>
-    Hello </div>
+  <NuxtPage />
 </template>
+
+<!-- prettier, tailwindcss, -->
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const route = useRoute()
+</script>
+
+<template>
+  <div>
+    <header>
+      <div>
+        <img src="/valiant.png" alt="Valiant FC logo">
+        <div>
+          <nav>
+            <RouterLink to="">Player</RouterLink>
+          </nav>
+        </div>
+      </div>
+    </header>
+  </div>
+</template>
