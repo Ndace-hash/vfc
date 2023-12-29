ca919843b2d9f4efdd489b9eb7ab8e786b85e429
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2023-12-29 08:48:07 +0100

    added tailwind and sass

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   app:{
     head:{
       link:[
-        {href: '/valiant.png', rel:'icon'}
+        {href: '/valiant.png', rel:'icon'},
+        {rel:"preconnect", href:"https://fonts.googleapis.com"},
+{
+  rel:'preconnect', href:'https://fonts.gstatic.com', crossorigin:""
+},
+{
+  href: 'https://fonts.googleapis.com/css2?family=Inter:wght@200;400;700;800&family=Smooch+Sans:wght@100;300;400;700&display=swap', rel: 'stylesheet'
+}
       ],
       title:"Valiant FC"
     }
-  }
+  },
+  css: ['./assets/css/main.scss'],
+  modules: ['@nuxtjs/tailwindcss']
 })
