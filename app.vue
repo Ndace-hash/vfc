c9af754c2fb0807c904efda2f49e20fb004ca7e1
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2024-01-03 20:55:30 +0100

    i added the footer section of the website

--- app.vue
+++ app.vue
@@ -15,6 +15,56 @@
     </header>
 
     <NuxtPage />
+    <footer class="flex justify-center flex-col items-center gap-6 mt-10">
+
+      <div class="flex items-center justify-center gap-6">
+        <div class="bg-primary flex items-center justify-center w-[40px] h-[40px] rounded-full overflow-hidden">
+          <RouterLink to="/">
+            <div class="w-[20px]">
+
+              <img src="~/assets/images/instagram.png" alt="" class="w-full h-full">
+            </div>
+          </RouterLink>
+        </div>
+        <div class="bg-primary flex items-center justify-center w-[40px] h-[40px] rounded-full overflow-hidden">
+          <RouterLink to="/">
+            <div class="w-[20px]">
+
+              <img src="~/assets/images/twitter.png" alt="" class="w-full h-full">
+            </div>
+          </RouterLink>
+        </div>
+        <div class="bg-primary flex items-center justify-center w-[40px] h-[40px] rounded-full overflow-hidden">
+          <RouterLink to="/">
+            <div class="w-[20px]">
+
+              <img src="~/assets/images/tiktok.png" alt="" class="w-full h-full">
+            </div>
+          </RouterLink>
+        </div>
+        <div class="bg-primary flex items-center justify-center w-[40px] h-[40px] rounded-full overflow-hidden">
+          <RouterLink to="/">
+            <div class="w-[15px]">
+
+              <img src="~/assets/images/facebook.png" alt="" class="w-full h-full">
+            </div>
+          </RouterLink>
+        </div>
+      </div>
+      <div class="flex items-center justify-center gap-6">
+        <RouterLink to="/">Club</RouterLink>
+        <RouterLink to="/">Contact us</RouterLink>
+        <RouterLink to="/">Shop</RouterLink>
+        <RouterLink to="/">Tour</RouterLink>
+        <RouterLink to="/">Policy</RouterLink>
+      </div>
+      <div class="w-[50px]">
+        <img src="/valiant.png" alt="" class="w-full h-full">
+      </div>
+      <div class="flex items-center justify-center bg-primary text-white font-semibold py-2 w-full">
+        <p>Valiant FC</p>
+      </div>
+    </footer>
   </main>
 </template>
 
