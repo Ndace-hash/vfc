f745bbcee3889b5271475b0c9b3c98300bb4b9f4
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2024-01-03 17:46:49 +0100

    moved the carosel slides to component

---
+++ components/CaroselItem.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="relative overflow-hidden mx-auto flex h-[300px] max-w-[800px]">
+        <div class="absolute z-0 flex items-center justify-center">
+            <img src="../assets/images/hero-news.jpg" alt="" class="w-full h-full">
+            <div class="absolute inset-0 bg-[#090808] bg-opacity-40"></div>
+        </div>
+        <div class="z-40 flex flex-col text-white px-10 justify-end py-4 gap-4 w-2/3">
+            <h2 class="capitalize font-bold text-3xl drop-shadow-lg">Headline of the day</h2>
+            <p class="text-base drop-shadow-xl">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit illum nobis
+                obcaecati porro?
+                Velit ex cum
+                perspiciatis deserunt dicta quis.</p>
+            <p class="text-sm drop-shadow-lg">1h ago</p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
