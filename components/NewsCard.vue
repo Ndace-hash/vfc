c7a32ffc2c8a7edd8890f12af26719d30f5757a5
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2024-01-05 20:23:35 +0100

    added responsiveness for news cards

--- components/NewsCard.vue
+++ components/NewsCard.vue
@@ -1,11 +1,13 @@
 <template>
     <article class="flex my-4 drop-shadow-[4px,4px,9px,0px] overflow-hidden">
-        <div class="w-1/4">
+        <div class="w-1/2 md:w-1/4">
             <img src="~/assets/images/news-card.jpg" alt="" class="w-full h-full">
         </div>
-        <div class="w-full ms-3 flex flex-col gap-4">
-            <p class="font-semibold text-lg">Lorem ipsum header</p>
-            <p class="max-w-[400px]">Hello what is happening? hyiipoklzik judosjd;lkj jsoioje jzjpodj lj zojdml jzjoi</p>
+        <div class="w-1/2 md:w-full ms-3 flex flex-col gap-3 md:gap-4 py-2">
+            <p class="font-semibold text-base md:text-lg">Lorem ipsum header</p>
+            <p class="max-w-[400px] text-xs md:text-base">Hello what is happening? hyiipoklzik judosjd;lkj jsoioje jzjpodj
+                lj zojdml
+                jzjoi</p>
             <p class="text-xs">1hr ago</p>
         </div>
     </article>
