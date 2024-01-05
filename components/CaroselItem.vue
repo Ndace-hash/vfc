76f5c71a50a04a5d1891b6e15c1a7061079f6869
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2024-01-05 20:15:39 +0100

    reponsive design for carosel item

--- components/CaroselItem.vue
+++ components/CaroselItem.vue
@@ -1,16 +1,14 @@
 <template>
-    <div class="relative overflow-hidden mx-auto flex h-[350px] max-w-[900px]">
-        <div class="absolute z-0 flex items-center justify-center">
-            <img src="../assets/images/hero-news.jpg" alt="" class="w-full h-full">
-            <div class="absolute inset-0 bg-[#090808] bg-opacity-40"></div>
-        </div>
-        <div class="z-40 flex flex-col text-white px-10 justify-end py-4 gap-4 w-2/3">
-            <h2 class="capitalize font-bold text-3xl drop-shadow-lg">Headline of the day</h2>
-            <p class="text-base drop-shadow-xl">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit illum nobis
+    <div class="relative overflow-hidden mx-auto flex h-[300px] md:h-[350px] max-w-[900px] carosel-wrapper">
+        <div class="absolute inset-0 bg-[#090808] bg-opacity-40"></div>
+        <div class="z-40 flex flex-col text-white px-10 justify-end py-4 gap-4 w-full md:w-2/3">
+            <h2 class="capitalize font-bold text-xl md:text-3xl drop-shadow-lg">Headline of the day</h2>
+            <p class=" text-sm md:text-base drop-shadow-xl">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit
+                illum nobis
                 obcaecati porro?
                 Velit ex cum
                 perspiciatis deserunt dicta quis.</p>
-            <p class="text-sm drop-shadow-lg">1h ago</p>
+            <p class="text-xs md:text-sm drop-shadow-lg">1h ago</p>
         </div>
     </div>
 </template>
@@ -19,3 +17,10 @@
 
 </script>
 
+<style lang="scss" scoped>
+.carosel-wrapper {
+    background-image: url(../assets/images/hero-news.jpg);
+    background-position: center;
+    background-size: cover;
+}
+</style>
