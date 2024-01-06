03e767fe05a82b53d6dac0031eb5d38af37eef22
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2024-01-06 12:20:47 +0100

    added sponsors section

--- pages/index.vue
+++ pages/index.vue
@@ -27,4 +27,12 @@ const route = useRoute()
     <MatchFixture />
     <button class="capitalize text-white bg-primary font-bold py-2 px-6 rounded-[8px] mt-4">view all fixtures</button>
   </section>
+  <section class=" max-w-[900px] mx-auto my-6 flex flex-col items-center">
+    <h2 class="uppercase font-bold">Sponsors</h2>
+    <div class="flex items-center justify-center gap-3 my-4">
+      <div class="w-[200px] border-2 border-primary rounded-[8px] p-2">
+        <img src="~/assets/images/island-casino.png" alt="" class="w-full h-full">
+      </div>
+    </div>
+  </section>
 </template>
