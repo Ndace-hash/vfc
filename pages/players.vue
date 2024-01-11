74b9d48957021aa1282343ef62adc17059c0dd15
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2024-01-11 11:24:59 +0100

    added player cards to the players page

---
+++ pages/players.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="flex overflow-scroll flex-nowrap">
+        <PlayerCard />
+        <PlayerCard />
+        <PlayerCard />
+        <PlayerCard />
+        <PlayerCard />
+        <PlayerCard />
+
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
