7cfa843337306875786277a1597d8b7d528e27cc
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2024-01-12 22:42:27 +0100

    prettier formatting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+	devtools: { enabled: true },
 
-  app:{
-    head:{
-      link:[
-        {href: '/valiant.png', rel:'icon'},
-        {rel:"preconnect", href:"https://fonts.googleapis.com"},
-{
-  rel:'preconnect', href:'https://fonts.gstatic.com', crossorigin:""
-},
-{
-  href: 'https://fonts.googleapis.com/css2?family=Inter:wght@200;400;700;800&family=Smooch+Sans:wght@100;300;400;700&display=swap', rel: 'stylesheet'
-}
-      ],
-      title:"Valiant FC"
-    }
-  },
-  modules: ['@nuxtjs/tailwindcss', "@nuxt/content", "nuxt-swiper"],
-  tailwindcss:{
-     configPath: './tailwind.config.js',
-  }
-})
+	app: {
+		head: {
+			link: [
+				{ href: "/valiant.png", rel: "icon" },
+				{ rel: "preconnect", href: "https://fonts.googleapis.com" },
+				{
+					rel: "preconnect",
+					href: "https://fonts.gstatic.com",
+					crossorigin: "",
+				},
+				{
+					href: "https://fonts.googleapis.com/css2?family=Inter:wght@200;400;700;800&family=Smooch+Sans:wght@100;300;400;700&display=swap",
+					rel: "stylesheet",
+				},
+			],
+			title: "Valiant FC",
+		},
+	},
+	modules: ["@nuxtjs/tailwindcss", "@nuxt/content", "nuxt-swiper"],
+	tailwindcss: {
+		configPath: "./tailwind.config.js",
+	},
+});
