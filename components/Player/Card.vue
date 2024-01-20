c0a59860b37624619ff58c5da1ca1a3cdee1f176
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2024-01-20 21:17:45 +0100

    display player cards

--- components/Player/Card.vue
+++ components/Player/Card.vue
@@ -1,22 +1,24 @@
 <template>
-    <article class="w-[250px] h-[300px] relative  rounded-[8px] shadow-xl overflow-hidden mx-2 my-4 flex-shrink-0">
+    <article class="w-[250px] h-[350px] relative  rounded-[8px] shadow-xl overflow-hidden mx-2 my-4 flex-shrink-0">
         <div class="v-logo-bg absolute inset-0 -z-10">
         </div>
-        <span class="absolute -z-10 -right-2 -top-2 text-7xl opacity-50 text-primary">22
+        <span class="absolute -z-10 -right-2 -top-2 text-7xl opacity-50 text-primary">{{ playerData.number }}
         </span>
         <div class="flex w-full h-full flex-col">
-            <div class="min-h-3/4 h-4/5 flex items-center justify-center">
-                <img src="/assets/images/island-casino.png" alt="" class="w-full h-full" />
+            <div class="h-3/4 flex items-center justify-center">
+                <img :src="playerData.photoURL" :alt="`Photo of ${playerData.first_name}`" class="w-full h-full" />
             </div>
             <div class="bg-white text-primary font-bold w-full px-2 py-4 justify-self-end">
-                <p class="text-lg">Zaccheaus</p>
-                <p class="text-3xl">Yisa</p>
+                <p class="text-lg">{{ playerData.first_name }}</p>
+                <p class="text-3xl">{{ playerData.last_name }}</p>
             </div>
         </div>
     </article>
 </template>
 
 <script setup lang="ts">
+import type { Player } from '~/types/Player'
+defineProps<{ playerData: Player }>()
 
 </script>
 
---
+++ components/Player/Display.vue
@@ -0,0 +1,19 @@
+<template>
+    <section class="my-6">
+        <h1 class="text-primary font-bold text-3xl px-4">{{ position }}</h1>
+        <div class="flex flex-wrap">
+            <PlayerCard v-for="player in playerList" :key="player.id" :player-data="player" />
+
+        </div>
+
+
+    </section>
+</template>
+
+<script setup lang="ts">
+import type { Player } from '~/types/Player';
+
+defineProps<{ position: string, playerList: Player[] }>()
+</script>
+
+<style scoped></style>
