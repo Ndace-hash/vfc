c82070849261c24fd3df6e83f7804aca8a8de257
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2024-01-24 10:43:36 +0100

    added editor functionalities

--- components/TipTap.client.vue
+++ components/TipTap.client.vue
@@ -1,63 +1,130 @@
 <template>
     <div>
-        <div v-if="editor">
+        <div v-if="editor" class="flex flex-wrap gap-2 mb-4">
             <UButton @click="editor.chain().focus().toggleBold().run()"
-                :disabled="!editor.can().chain().focus().toggleBold().run()"
-                :class="{ 'is-active': editor.isActive('bold') }">
-                <UIcon name="i-material-symbols-format-bold" dynamic />
+                :disabled="!editor.can().chain().focus().toggleBold().run()" class=" border border-primary" size="xs"
+                :variant="editor.isActive('bold') ? 'solid' : 'ghost'">
+                <UIcon name="i-material-symbols-format-bold" dynamic class="text-2xl" />
             </UButton>
             <UButton @click="editor.chain().focus().toggleItalic().run()"
-                :disabled="!editor.can().chain().focus().toggleItalic().run()"
-                :class="{ 'is-active': editor.isActive('italic') }">
-                <UIcon name="i-material-symbols-format-italic" dynamic />
+                :disabled="!editor.can().chain().focus().toggleItalic().run()" class=" border border-primary" size="xs"
+                :variant="editor.isActive('italic') ? 'solid' : 'ghost'">
+                <UIcon name="i-material-symbols-format-italic" dynamic class="text-2xl" />
             </UButton>
-            <UButton @click="editor.chain().focus().setParagraph().run()"
-                :class="{ 'is-active': editor.isActive('paragraph') }">
-                paragraph
+            <UButton @click="editor.chain().focus().toggleHeading({ level: 1 }).run()" class=" border border-primary"
+                size="xs" :variant="editor.isActive('heading', { level: 1 }) ? 'solid' : 'ghost'">
+                H1
+            </UButton>
+            <UButton @click="editor.chain().focus().toggleHeading({ level: 2 }).run()" class=" border border-primary"
+                size="xs" :variant="editor.isActive('heading', { level: 2 }) ? 'solid' : 'ghost'">
+                H2
             </UButton>
             <UButton @click="editor.chain().focus().toggleHeading({ level: 1 }).run()"
                 :class="{ 'is-active': editor.isActive('heading', { level: 1 }) }">
-                H1
+                <UIcon name="i-material-symbols-add-link" dynamic class="text-2xl" />
             </UButton>
-            <UButton @click="editor.chain().focus().toggleHeading({ level: 2 }).run()"
-                :class="{ 'is-active': editor.isActive('heading', { level: 2 }) }">
-                H2
+            <UButton @click="editor.chain().focus().toggleBulletList().run()" class=" border border-primary" size="xs"
+                :variant="editor.isActive('bulletList') ? 'solid' : 'ghost'">
+                <UIcon name="i-material-symbols-format-list-bulleted" dynamic class="text-2xl" />
             </UButton>
-            <UButton @click="editor.chain().focus().toggleBulletList().run()"
-                :class="{ 'is-active': editor.isActive('bulletList') }">
-                <UIcon name="i-material-symbols-format-list-bulleted" dynamic />
+            <UButton @click="editor.chain().focus().toggleOrderedList().run()" class=" border border-primary" size="xs"
+                :variant="editor.isActive('orderedList') ? 'solid' : 'ghost'">
+                <UIcon name="i-material-symbols-format-list-numbered" dynamic class="text-2xl" />
             </UButton>
-            <UButton @click="editor.chain().focus().toggleOrderedList().run()"
-                :class="{ 'is-active': editor.isActive('orderedList') }">
-                <UIcon name="i-material-symbols-format-list-numbered" dynamic />
-            </UButton>
-            <UButton @click="editor.chain().focus().toggleBlockquote().run()"
-                :class="{ 'is-active': editor.isActive('blockquote') }">
+            <UButton @click="editor.chain().focus().toggleBlockquote().run()" class=" border border-primary" size="xs"
+                :variant="editor.isActive('blockquote') ? 'solid' : 'ghost'">
 
-                <UIcon name="i-tabler-blockquote" dynamic />
+                <UIcon name="i-tabler-blockquote" dynamic class="text-2xl" />
             </UButton>
             <UButton @click="editor.chain().focus().setHorizontalRule().run()">
-                <UIcon name="i-carbon-ruler" dynamic />
+                <UIcon name="i-carbon-ruler" dynamic class="text-2xl" />
             </UButton>
-            <button @click="editor.chain().focus().setHardBreak().run()">
-                hard break
-            </button>
             <UButton @click="editor.chain().focus().undo().run()" :disabled="!editor.can().chain().focus().undo().run()">
-                <UIcon name="i-ic-baseline-undo" dynamic />
+                <UIcon name="i-ic-baseline-undo" dynamic class="text-2xl" />
             </UButton>
             <UButton @click="editor.chain().focus().redo().run()" :disabled="!editor.can().chain().focus().redo().run()">
-                <UIcon name="i-ic-baseline-redo" dynamic />
+                <UIcon name="i-ic-baseline-redo" dynamic class="text-2xl" />
             </UButton>
         </div>
-        <EditorContent :editor="editor" />
+        <EditorContent :editor="editor"
+            class="min-h-[30rem] max-h-[30rem] overflow-y-auto bg-slate-100 outline-none h-full p-2 tippy" />
+
     </div>
 </template>
 
 <script setup>
 import { useEditor, EditorContent } from '@tiptap/vue-3'
 import TiptapStarterKit from '@tiptap/starter-kit'
+import Link from '@tiptap/extension-link';
 const editor = useEditor({
     content: "<p>I'm running Tiptap with Vue.js. 🎉</p>",
-    extensions: [TiptapStarterKit],
+    extensions: [TiptapStarterKit.configure({
+        heading: {
+            HTMLAttributes: {
+                class: 'heading'
+            },
+        },
+        bulletList: {
+            HTMLAttributes: {
+                class: 'list-item'
+            }
+        },
+        orderedList: {
+            HTMLAttributes: {
+                class: 'list-item'
+            }
+        }
+    }), Link,],
+    autofocus: true,
+
 });
 </script>
+
+<style lang="scss">
+.tippy {
+    .tiptap.ProseMirror-focused {
+        border: 1px solid transparent;
+        outline: none;
+
+        &>p {
+            @apply mb-3;
+        }
+    }
+}
+
+ul {
+    @apply list-disc;
+}
+
+h1.heading {
+    @apply text-3xl font-bold
+}
+
+h2.heading {
+    @apply text-xl font-semibold;
+}
+
+ul {
+    @apply mb-4;
+
+    &.list-item li {
+        @apply list-disc ms-6;
+    }
+}
+
+ol {
+    @apply mb-4;
+
+    &.list-item li {
+        @apply list-decimal ms-6;
+    }
+}
+
+blockquote {
+    @apply ms-4 border-l-2 border-l-primary py-1 px-2;
+}
+
+.is-active {
+    @apply text-white bg-primary;
+}
+</style>
---
+++ pages/admin/news/create.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="flex items-center justify-center h-screen">
+        <TipTap class=" bg-white w-[80%] mx-auto rounded-[8px] py-6 px-8" />
+    </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+    layout: 'admin'
+})
+</script>
+
+<style scoped></style>
