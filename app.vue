a5a06f5045fe0fbfad44c81f00493411321f9f3d
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2024-02-01 13:59:54 +0100

    ability to add fixtures to the database

--- app.vue
+++ app.vue
@@ -6,3 +6,17 @@
     </NuxtLayout>
   </div>
 </template> 
+
+<script setup>
+import { getDocs, where, query, collection } from 'firebase/firestore'
+import { fireStore } from '~/config/firebase'
+const valiant = useState('valiant')
+
+const q = query(collection(fireStore, 'clubs'), where('name', '==', 'Valiant FC'))
+getDocs(q).then(snapshot => {
+  const doc = snapshot.docs[0]
+
+  valiant.value = { id: doc.id, logo: doc.data().logo, name: doc.data().name }
+})
+
+</script>
--- pages/admin/fixture.vue
+++ pages/admin/fixture.vue
@@ -1,5 +1,29 @@
 <template>
     <section>
+        <div>
+            <h1 class="text-white capitalize text-2xl max-w-[1024px] mx-auto my-6">Fixtures</h1>
+            <div
+                class="bg-white max-w-[1024px] h-full max-h-[600px] mx-auto rounded-[8px] shadow-lg my-10 overflow-y-hidden">
+                <div class="flex items-center py-4 px-4 justify-between border-b border-admin-light">
+                    <button
+                        class="flex bg-primary text-white border-2 border-primary items-center justify-center gap-1   font-semibold py-2 px-4 rounded-full md:hover:shadow-md duration-200 ease-in "
+                        @click="isOpen = true">
+                        <span>Add Fixtures</span>
+                        <IconPlus width="30px" color="#ffffff" />
+                    </button>
+                    <div class="flex gap-2 items-center">
+                        <UButton
+                            class="bg-transparent text-admin-dark flex items-center border-2 border-primary hover:text-white"
+                            :disabled="isRefreshing">
+                            <IconRefresh width="15px"
+                                :class="isRefreshing ? 'duration-1000 ease-linear animate-spin' : ''" />
+                            Refresh
+                        </UButton>
+                    </div>
+                </div>
+            </div>
+
+        </div>
 
         <UModal v-model="isOpen">
             <UCard>
@@ -28,7 +52,10 @@
                                 </template>
                             </UInputMenu>
                         </UFormGroup>
-                        <UFormGroup label="Match Day" class="mb-4">
+                        <UFormGroup label="Season" class="mb-4">
+                            <UInput placeholder="Ex. 2023/2024" v-model="state.season" />
+                        </UFormGroup>
+                        <UFormGroup label="Match Day" class="mb-4 flex items-center flex-col">
                             <DatePicker mode="dateTime" v-model="state.date" />
                         </UFormGroup>
                         <UFormGroup class="flex justify-end">
@@ -43,25 +70,59 @@
 
 <script setup lang="ts">
 import { fireStore } from '~/config/firebase'
-import { collection, getDocs } from 'firebase/firestore'
+import { collection, getDocs, addDoc } from 'firebase/firestore'
 import { DatePicker } from 'v-calendar'
 definePageMeta({
     layout: 'admin'
 })
-const isOpen = ref(true)
+const valiant = useState('valiant')
+const isOpen = ref(false)
+const isRefreshing = ref(false)
 const state = reactive({
     venue: '',
     date: new Date(),
-    oponent: {}
+    oponent: { id: '', logo: '', name: '' },
+    season: ''
 })
 const clubs = ref<any>([])
 const options = [{
     label: 'Home',
     value: 'home'
 }, { label: 'Away', value: 'away' }]
-
 const addFixture = () => {
-    console.log(state)
+    const fixture = ref({
+        home: {
+            id: '',
+            logo: '',
+            name: ''
+        },
+        away: {
+            id: '',
+            logo: '',
+            name: ''
+        },
+        season: state.season.trim().replace('/', '_'), played: false,
+        goals: {
+            home: 0,
+            away: 0,
+        },
+        date: state.date
+    })
+    if (state.venue == 'home') {
+        fixture.value.away = state.oponent
+        fixture.value.home = valiant.value
+    } else if (state.venue == 'away') {
+        fixture.value.home = state.oponent
+        fixture.value.away = valiant.value
+
+    }
+    try {
+        addDoc(collection(fireStore, 'fixtures'), fixture.value).then((snapshot) => {
+            console.log(snapshot)
+        })
+    } catch (e) {
+        console.error(e)
+    }
 }
 
 onBeforeMount(() => {
