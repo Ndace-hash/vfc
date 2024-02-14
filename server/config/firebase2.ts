679940b6484c2bf793925b364ba3bf0dd05e921e
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2024-02-14 11:17:15 +0100

    nuxt server firase config

---
+++ server/config/firebase2.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
+// import { getStorage } from "firebase/storage";
+// const runtimeConfig = useRuntimeConfig();
+// const firebaseConfig = {
+// 	apiKey: runtimeConfig.apiKey,
+// 	authDomain: runtimeConfig.authDomain,
+// 	projectId: runtimeConfig.projectId,
+// 	storageBucket: runtimeConfig.storageBucket,
+// 	messagingSenderId: runtimeConfig.messagingSenderId,
+// 	appId: runtimeConfig.appId,
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+// // initialize firestore
+// export const fireStore = getFirestore(app);
+// export const storage = getStorage(app);
