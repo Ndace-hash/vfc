52a7f41df0186ec4309ba0fe483f0eb7c4ade544
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2024-02-23 14:35:33 +0100

    created pinia store for users

--- stores/Users.ts
+++ stores/Users.ts
@@ -1,22 +1,43 @@
-import { signInWithEmailAndPassword, type User } from "firebase/auth";
+import { signInWithEmailAndPassword, type User, signOut } from "firebase/auth";
 import { auth } from "~/config/firebase";
 import type { LoginDetails } from "~/types/Auth";
 import { useStorage } from "@vueuse/core";
 export const useUserStore = defineStore("user", () => {
-	const currentUser = useStorage<User>("currentUser", null);
+	const currentUser = ref<User | null>(null);
+	const localUser = localStorage.getItem("currentUser");
+	if (localUser) {
+		currentUser.value = JSON.parse(localUser);
+	}
 
+	const isAdmin = ref(false);
 	const setUser = async (state: LoginDetails) => {
 		try {
-			const userCredential = await signInWithEmailAndPassword(
-				auth,
-				state.email,
-				state.password
+			await signInWithEmailAndPassword(auth, state.email, state.password).then(
+				(userCredential) => {
+					currentUser.value = userCredential.user;
+					localStorage.setItem(
+						"currentUser",
+						JSON.stringify(userCredential.user)
+					);
+				}
 			);
-			currentUser.value = userCredential.user;
 		} catch (e) {
 			console.error(e);
 		}
 	};
+	const unSetUser = async () => {
+		try {
+			signOut(auth).then(() => {
+				currentUser.value = null;
+			});
+		} catch (e) {
+			console.error(e);
+		}
+	};
+	function $reset() {
+		currentUser.value = null;
+		isAdmin.value = false;
+	}
 
-	return { currentUser, setUser };
+	return { currentUser, setUser, unSetUser, isAdmin, $reset };
 });
