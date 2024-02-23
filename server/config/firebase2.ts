e2dc3b06fd6a9c19978d2f756820d8e8424a1bc7
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2024-02-23 14:34:58 +0100

    server firebase config

--- server/config/firebase2.ts
+++ server/config/firebase2.ts
@@ -2,15 +2,15 @@
 // import { initializeApp } from "firebase/app";
 // import { getFirestore } from "firebase/firestore";
 // import { getStorage } from "firebase/storage";
-// const runtimeConfig = useRuntimeConfig();
-// const firebaseConfig = {
-// 	apiKey: runtimeConfig.apiKey,
-// 	authDomain: runtimeConfig.authDomain,
-// 	projectId: runtimeConfig.projectId,
-// 	storageBucket: runtimeConfig.storageBucket,
-// 	messagingSenderId: runtimeConfig.messagingSenderId,
-// 	appId: runtimeConfig.appId,
-// };
+const runtimeConfig = useRuntimeConfig();
+const firebaseConfig = {
+	apiKey: runtimeConfig.apiKey,
+	authDomain: runtimeConfig.authDomain,
+	projectId: runtimeConfig.projectId,
+	storageBucket: runtimeConfig.storageBucket,
+	messagingSenderId: runtimeConfig.messagingSenderId,
+	appId: runtimeConfig.appId,
+};
 
 // // Initialize Firebase
 // const app = initializeApp(firebaseConfig);
@@ -18,3 +18,8 @@
 // // initialize firestore
 // export const fireStore = getFirestore(app);
 // export const storage = getStorage(app);
+
+import { initializeApp } from "firebase-admin/app";
+import { getAuth } from "firebase-admin/auth";
+const adminApp = initializeApp();
+export const authApp = getAuth();
