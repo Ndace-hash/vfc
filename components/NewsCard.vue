68dbf260b4ddfd209c6d8d77e432793081c1604d
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2024-03-08 21:45:26 +0100

    truncate text on the card

--- components/NewsCard.vue
+++ components/NewsCard.vue
@@ -3,18 +3,24 @@
         <div class="w-1/2 md:w-1/4">
             <img src="~/assets/images/news-card.jpg" alt="" class="w-full h-full">
         </div>
-        <div class="w-1/2 md:w-full ms-3 flex flex-col gap-3 md:gap-4 py-2">
-            <p class="font-semibold text-base md:text-lg">Lorem ipsum header</p>
-            <p class="max-w-[400px] text-xs md:text-base">Hello what is happening? hyiipoklzik judosjd;lkj jsoioje
-                jzjpodj
-                lj zojdml
-                jzjoi</p>
-            <p class="text-xs">1hr ago</p>
+        <div class="w-1/2 md:w-full ms-3 flex flex-col gap-3 md:gap-4 py-2 h-28">
+            <p class="font-semibold text-base md:text-lg">{{ article.title }}</p>
+            <p class="max-w-[550px] text-xs md:text-base overflow-y-hidden text-wrap min-h-20" v-html="trimedHTML()"
+                :style="{ textOverflow: 'ellipsis' }">
+            </p>
         </div>
     </article>
 </template>
 
 <script setup lang="ts">
+import * as cheerio from 'cheerio'
+import type { Article } from '~/types/News'
+const props = defineProps<{ article: Article }>()
+const trimedHTML = () => {
+    const $ = cheerio.load(props.article.content)
+    return $('p:first').text()
+
+}
 </script>
 
 <style lang="scss" scoped>
