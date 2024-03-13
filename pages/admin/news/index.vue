740c1dc1f472b8337db656de4d42dfb683b8c9f7
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2024-03-13 13:09:31 +0100

    change the news page into the create page where admin can publish news

--- pages/admin/news/index.vue
+++ pages/admin/news/index.vue
@@ -1,33 +1,63 @@
 <template>
-    <section>
-        <h1 class="text-white capitalize text-2xl max-w-[1024px] mx-auto my-6">News <span
-                class="font-semibold">Updates</span></h1>
-        <div class="bg-white max-w-[1024px] h-full max-h-[600px] mx-auto rounded-[8px] shadow-lg my-10 overflow-y-hidden">
-            <div class="flex items-center py-4 px-4 justify-between border-b border-admin-light">
-                <NuxtLink to="/admin/news/create"
-                    class="flex bg-primary text-white border-2 border-primary items-center justify-center gap-1   font-semibold py-2 px-4 rounded-full md:hover:shadow-md duration-200 ease-in ">
-                    <span>Create</span>
-                    <IconPlus width="30px" color="#ffffff" />
-                </NuxtLink>
-                <div class="flex gap-2 items-center">
-                    <UButton
-                        class="bg-transparent text-admin-dark flex items-center border-2 border-primary hover:text-white">
-                        <IconRefresh width="15px" />
-                        Refresh
-                    </UButton>
-                    <UInput placeholder="filter news..." />
-                </div>
-            </div>
 
-        </div>
+    <Head>
+        <Title>Publish News | Valiant FC</Title>
+    </Head>
+    <div class="flex items-center justify-center  max-w-[1024px] w-full">
+        <UForm :state="state" class="flex flex-col gap-6" @submit="publish">
+            <UFormGroup class="w-full mx-auto" required size="lg">
+                <template #label>
+                    <p class="inline text-white font-semibold text-xl  me-1">Title</p>
+                </template>
+                <UInput v-model="state.title" placeholder="New update on Valiant FC" class="mt-2" />
+            </UFormGroup>
+            <UFormGroup class="flex flex-col gap-2">
+                <template #label>
+                    <p class="inline text-white font-semibold text-sm  me-1">Add Banner Image</p>
 
-    </section>
+                </template>
+                <template #default>
+
+                    <label
+                        class="text-white border-2 border-primary px-4 py-1 shadow-md rounded-md bg-primary font-bold flex items-center gap-2 w-max"
+                        for="banner">
+                        <UIcon name="i-material-symbols-drive-folder-upload-outline-rounded" dynamic class="text-lg" />
+                        Upload
+                    </label>
+                    <input type="file" accept=".jpg,.png,.jpeg" id="banner" class="hidden" />
+                </template>
+            </UFormGroup>
+            <UFormGroup>
+                <TipTap class=" bg-white w-full mx-auto rounded-[8px] py-6 px-8" v-model="state.content" />
+
+            </UFormGroup>
+            <UFormGroup class=" flex justify-end">
+
+                <UButton class="inline-block font-bold text-lg px-12 shadow-md " size="lg" type="submit">Publish
+                </UButton>
+            </UFormGroup>
+        </UForm>
+
+    </div>
 </template>
 
 <script setup lang="ts">
+import { fireStore } from '~/config/firebase'
+import { collection, addDoc } from 'firebase/firestore'
+
 definePageMeta({
     layout: 'admin'
 })
+const state = reactive({
+    title: '',
+    content: ''
+})
+
+const publish = () => {
+    addDoc(collection(fireStore, 'news'), state).then(snapshot => {
+        console.log(snapshot)
+    })
+}
 </script>
 
 <style scoped></style>
