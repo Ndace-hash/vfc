a8a9f6155c2f6fbe8e6c3f2109ae1109651d9856
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2024-03-14 19:25:18 +0100

    added limit and order by query

--- server/api/get-news.get.ts
+++ server/api/get-news.get.ts
@@ -1,10 +1,15 @@
-import { getDocs, collection } from "firebase/firestore";
+import { getDocs, collection, limit, orderBy, query } from "firebase/firestore";
 import { fireStore } from "../config/firebase2";
 
 export default defineEventHandler(async () => {
 	try {
+		const q = query(
+			collection(fireStore, "news"),
+			orderBy("publishedAt", "desc"),
+			limit(10)
+		);
 		const data: any = [];
-		const querySnapShot = await getDocs(collection(fireStore, "news"));
+		const querySnapShot = await getDocs(q);
 		querySnapShot.docs.forEach((doc) => {
 			data.push({
 				id: doc.id,
