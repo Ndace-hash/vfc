bb3f2dd918d2ff48345ae57d015c2fac25c4463e
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2024-03-14 19:18:18 +0100

    added routing for slider articles

--- components/CaroselItem.vue
+++ components/CaroselItem.vue
@@ -1,19 +1,30 @@
 <template>
-    <div class="relative overflow-hidden mx-auto flex h-[300px] md:h-[350px] max-w-[900px] carosel-wrapper">
+    <div class="relative overflow-hidden mx-auto flex h-[300px] md:h-[350px] max-w-[900px] carosel-wrapper"
+        @click="viewNews(article.id)">
         <div class="absolute inset-0 bg-[#090808] bg-opacity-40"></div>
         <div class="z-40 flex flex-col text-white px-10 justify-end py-4 gap-4 w-full md:w-2/3">
-            <h2 class="capitalize font-bold text-xl md:text-3xl drop-shadow-lg">Headline of the day</h2>
-            <p class=" text-sm md:text-base drop-shadow-xl">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit
-                illum nobis
-                obcaecati porro?
-                Velit ex cum
-                perspiciatis deserunt dicta quis.</p>
+            <h2 class="capitalize font-bold text-xl md:text-3xl drop-shadow-lg">{{ article.title }}</h2>
+            <p class=" text-sm md:text-base drop-shadow-xl" v-html="trimedHTML()"></p>
             <p class="text-xs md:text-sm drop-shadow-lg">1h ago</p>
         </div>
     </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import * as cheerio from 'cheerio'
+import type { Article } from '~/types/News'
+const router = useRouter()
+const props = defineProps<{ article: Article }>()
+const viewNews = (id: string) => {
+    router.push({
+        path: `/news/${id}`
+    })
+}
+const trimedHTML = () => {
+    const $ = cheerio.load(props.article.content)
+    return $('p:first').text()
+
+}
 
 </script>
 
