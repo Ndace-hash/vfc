0a43a822cf372b4a001e90937c56f30c576070ef
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2024-03-15 12:19:37 +0100

    format published at date

--- components/CaroselItem.vue
+++ components/CaroselItem.vue
@@ -4,8 +4,8 @@
         <div class="absolute inset-0 bg-[#090808] bg-opacity-40"></div>
         <div class="z-40 flex flex-col text-white px-10 justify-end py-4 gap-4 w-full md:w-2/3">
             <h2 class="capitalize font-bold text-xl md:text-3xl drop-shadow-lg">{{ article.title }}</h2>
-            <p class=" text-sm md:text-base drop-shadow-xl" v-html="trimedHTML()"></p>
-            <p class="text-xs md:text-sm drop-shadow-lg">1h ago</p>
+            <p class=" text-sm md:text-base drop-shadow-xl overflow-hidden h-12" v-html="trimedHTML()"></p>
+            <p class="text-xs md:text-sm drop-shadow-lg font-semibold">{{ article.publishedAt }}</p>
         </div>
     </div>
 </template>
@@ -20,6 +20,14 @@ const viewNews = (id: string) => {
         path: `/news/${id}`
     })
 }
+const article = ref({
+    ...props.article,
+    publishedAt: (() => {
+        let nano = props.article.publishedAt.nanoseconds * (10 ** -9)
+        let milliSeconds = (props.article.publishedAt.seconds + nano) * 1000
+        return new Date(milliSeconds).toDateString()
+    })()
+})
 const trimedHTML = () => {
     const $ = cheerio.load(props.article.content)
     return $('p:first').text()
