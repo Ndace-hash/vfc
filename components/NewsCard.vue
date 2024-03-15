adfb8b7d414160a87f6ab091fa3017563f8317e9
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2024-03-15 12:35:58 +0100

    display banner for the articles

--- components/NewsCard.vue
+++ components/NewsCard.vue
@@ -1,7 +1,7 @@
 <template>
     <article class="flex my-4 drop-shadow-[4px,4px,9px,0px] overflow-hidden" @click="viewNews(article.id)">
         <div class="w-1/2 md:w-1/4">
-            <img src="~/assets/images/news-card.jpg" alt="" class="w-full h-full">
+            <img :src="article.bannerUrl" :alt="article.title" class="w-full h-full">
         </div>
         <div class="w-1/2 md:w-full ms-3 flex flex-col gap-3 md:gap-4 py-2 h-28">
             <p class="font-semibold text-base md:text-lg">{{ article.title }}</p>
