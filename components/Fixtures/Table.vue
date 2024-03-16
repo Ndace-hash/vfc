4c3ea2d6ff4bdf33db7aee7af796ae8e2d1e75f2
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2024-03-16 12:58:47 +0100

    display the league standings

---
+++ components/Fixtures/Table.vue
@@ -0,0 +1,15 @@
+<template>
+    <section class=" max-w-[900px] mx-auto my-6 flex flex-col items-center">
+        <h2 class="uppercase font-bold">Standings</h2>
+        <div class="flex items-center justify-center gap-3 my-4">
+            <LeagueTable :team-stat="data" />
+        </div>
+    </section>
+</template>
+
+<script setup lang="ts">
+const { data } = useFetch<TeamStat[]>('/api/leagueTable')
+
+</script>
+
+<style scoped></style>
