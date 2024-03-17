bbda7ed974f96118bbc58dad176ab3ee693622ad
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2024-03-17 01:00:29 +0100

    display fixtures and results

---
+++ components/Fixtures/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <FixturesCard :game="game" v-for="game in matches.fixtures" :key="game.id" />
+    </div>
+</template>
+
+<script setup lang="ts">
+const matches = useFixtures()
+</script>
+
+<style scoped></style>
