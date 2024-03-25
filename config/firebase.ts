e38da55ce5a9b7fd8e856668377ae2fc0a79436e
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2024-03-25 14:45:37 +0100

    added runtime config env

--- config/firebase.ts
+++ config/firebase.ts
@@ -1,17 +1,19 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp, type FirebaseOptions } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
+const runtimeConfig = useRuntimeConfig();
+
 const firebaseConfig = {
-	apiKey: "AIzaSyCiKk72rfOWD3gvbFKjGYdbb0uCs4NwW80",
-	authDomain: "valiant-fc-dev.firebaseapp.com",
-	projectId: "valiant-fc-dev",
-	storageBucket: "valiant-fc-dev.appspot.com",
-	messagingSenderId: "641701082658",
-	appId: "1:641701082658:web:64bd0879e02acc99c6fc7b",
-};
+	apiKey: runtimeConfig.apiKey,
+	authDomain: runtimeConfig.authDomain,
+	projectId: runtimeConfig.projectId,
+	storageBucket: runtimeConfig.storageBucket,
+	messagingSenderId: runtimeConfig.messagingSenderId,
+	appId: runtimeConfig.appId,
+} as FirebaseOptions;
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
