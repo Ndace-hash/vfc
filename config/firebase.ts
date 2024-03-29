20b74d5f71c8bda106d771d2a05bbde9482f6059
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2024-03-29 21:51:37 +0100

    set process env variables in firebase config

--- config/firebase.ts
+++ config/firebase.ts
@@ -4,15 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
-const runtimeConfig = useRuntimeConfig();
-
 const firebaseConfig = {
-	apiKey: runtimeConfig.apiKey,
-	authDomain: runtimeConfig.authDomain,
-	projectId: runtimeConfig.projectId,
-	storageBucket: runtimeConfig.storageBucket,
-	messagingSenderId: runtimeConfig.messagingSenderId,
-	appId: runtimeConfig.appId,
+	apiKey: process.env.NUXT_API_KEY,
+	authDomain: process.env.NUXT_AUTH_DOMAIN,
+	projectId: process.env.NUXT_PROJECT_ID,
+	storageBucket: process.env.NUXT_STORAGE_BUCKET,
+	messagingSenderId: process.env.NUXT_MESSAGING_SENDER_ID,
+	appId: process.env.APP_ID,
 } as FirebaseOptions;
 
 // Initialize Firebase
