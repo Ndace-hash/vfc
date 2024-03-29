da119e3ff0e23532ce0733bba3bb6601fbc30480
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2024-03-29 22:28:06 +0100

    added default values

--- config/firebase.ts
+++ config/firebase.ts
@@ -5,12 +5,12 @@ import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-	apiKey: process.env.NUXT_API_KEY,
-	authDomain: process.env.NUXT_AUTH_DOMAIN,
-	projectId: process.env.NUXT_PROJECT_ID,
-	storageBucket: process.env.NUXT_STORAGE_BUCKET,
-	messagingSenderId: process.env.NUXT_MESSAGING_SENDER_ID,
-	appId: process.env.APP_ID,
+	apiKey: process.env.API_KEY || "AIzaSyCiKk72rfOWD3gvbFKjGYdbb0uCs4NwW80",
+	authDomain: process.env.AUTH_DOMAIN || "valiant-fc-dev.firebaseapp.com",
+	projectId: process.env.PROJECT_ID || "valiant-fc-dev",
+	storageBucket: process.env.STORAGE_BUCKET || "valiant-fc-dev.appspot.com",
+	messagingSenderId: process.env.MESSAGING_SENDER_ID || "641701082658",
+	appId: process.env.APP_ID || "1:641701082658:web:64bd0879e02acc99c6fc7b",
 } as FirebaseOptions;
 
 // Initialize Firebase
